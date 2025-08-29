1fbb03f6d99798de2904763d2d0d80784f1bcc67
可我真的什么都不会啊 <151507264+azusa-dom@users.noreply.github.com>
2025-08-29 04:03:37 +0000

    fix: 修复路由和语言上下文问题

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,21 @@ import StudentDashboard from './components/student/StudentDashboard'
 
 function App() {
   return (
-    <LanguageProvider>
-      <ThemeProvider>
-        <AppProvider>
-          <UserProvider>
-            <Router basename="/Student_app">
+    <Router basename="/Student_app">
+      <LanguageProvider>
+        <ThemeProvider>
+          <AppProvider>
+            <UserProvider>
               <Routes>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/parent/dashboard" element={<ParentDashboard />} />
                 <Route path="/student/dashboard" element={<StudentDashboard />} />
               </Routes>
-            </Router>
-          </UserProvider>
-        </AppProvider>
-      </ThemeProvider>
-    </LanguageProvider>
+            </UserProvider>
+          </AppProvider>
+        </ThemeProvider>
+      </LanguageProvider>
+    </Router>
   )
 }
 
