7f5d3b2ebcf2671101541c0a614e397b7f9aa207
可我真的什么都不会啊 <151507264+azusa-dom@users.noreply.github.com>
2025-08-29 00:33:57 +0000

    improve: enhance settings page with cancel button and save confirmation

--- dist/assets/helpers-C_TRis_w.js
+++ dist/assets/helpers-C_TRis_w.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-NeV98MkW.js";/**
+import{c as e}from"./index-5RqCyBfh.js";/**
  * @license lucide-react v0.309.0 - ISC
  *
  * This source code is licensed under the ISC license.
